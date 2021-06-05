65f07056b1a3ef54c01d010787b07e0d0d5ec263
k_yamamoto <k_yamamoto@ksyssol.co.jp>
2021-06-05 15:28:17 +0900

    format

--- src/statement.test.js
+++ src/statement.test.js
@@ -1,29 +1,29 @@
-const statement = require('./statement');
+const statement = require("./statement");
 const plays = {
-  "hamlet": {"name":"Hamlet", "type": "tragedy"},
-  "as-like": {"name": "As You Like It", "type": "comedy"},
-  "othello": {"name": "Othello","type": "tragedy"}
+  hamlet: { name: "Hamlet", type: "tragedy" },
+  "as-like": { name: "As You Like It", type: "comedy" },
+  othello: { name: "Othello", type: "tragedy" },
 };
 
 const invoices = {
-      "customer": "BigCo",
-      "performances": [
-          {
-              "playID": "hamlet",
-              "audience": 55
-          },
-          {
-              "playID": "as-like",
-              "audience": 35
-          },
-          {
-              "playID": "othello",
-              "audience": 40
-          }
-      ]
-  };
+  customer: "BigCo",
+  performances: [
+    {
+      playID: "hamlet",
+      audience: 55,
+    },
+    {
+      playID: "as-like",
+      audience: 35,
+    },
+    {
+      playID: "othello",
+      audience: 40,
+    },
+  ],
+};
 
-test('statementの実行', () => {
+test("statementの実行", () => {
   expect(statement(invoices, plays)).toBe(`Statement for BigCo
  Hamlet: $650.00 (55 seats)
  As You Like It: $580.00 (35 seats)
