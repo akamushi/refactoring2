6784eef65f46baab85a2a6a5e16dc194dbb49f9d
k_yamamoto <k_yamamoto@ksyssol.co.jp>
2021-06-06 14:30:09 +0900

    関数を入れ子にして引数を削除

--- src/statement.js
+++ src/statement.js
@@ -2,65 +2,72 @@
 
 function statement(invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
 
   for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(plays, perf);
     // 注文の内訳を出力
-    result += ` ${playFor(plays, perf).name}: ${usd(amountFor(plays, perf))} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     } seats)\n`;
-    totalAmount += amountFor(plays, perf);
+    totalAmount += amountFor(perf);
   }
+  let volumeCredits = totalVolumeCredits();
   result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
-}
 
-function usd(aNumber) {
-  return new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format(aNumber / 100);
-}
-
-function volumeCreditsFor(plays, aPerformance) {
-  let result = 0;
-  // ボリューム得点のポイントを加算
-  result += Math.max(aPerformance.audience - 30, 0);
-  // 喜劇の時は10人につき、さらにポイントを加算
-  if ("comedy" === playFor(plays, aPerformance).type)
-    result += Math.floor(aPerformance.audience / 5);
-  return result;
-}
-
-function playFor(plays, aPerformance) {
-  return plays[aPerformance.playID];
-}
-
-function amountFor(plays, aPerformance) {
-  let result = 0;
-  switch (playFor(plays, aPerformance).type) {
-    case "tragedy":
-      result = 40000;
-      if (aPerformance.audience > 30) {
-        result += 1000 * (aPerformance.audience - 30);
-      }
-      break;
-    case "comedy":
-      result = 30000;
-      if (aPerformance.audience > 20) {
-        result += 10000 + 500 * (aPerformance.audience - 20);
-      }
-      result += 300 * aPerformance.audience;
-      break;
-
-    default:
-      throw new Error(`unknown type: ${playFor(plays, aPerformance).type}`);
+  function totalVolumeCredits() {
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
+    }
+    return volumeCredits;
   }
-  return result;
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
+  }
+
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    // ボリューム得点のポイントを加算
+    result += Math.max(aPerformance.audience - 30, 0);
+    // 喜劇の時は10人につき、さらにポイントを加算
+    if ("comedy" === playFor(aPerformance).type)
+      result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
+  function amountFor(aPerformance) {
+    let result = 0;
+    switch (playFor(aPerformance).type) {
+      case "tragedy":
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy":
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+
+      default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+    }
+    return result;
+  }
 }
 
 module.exports = statement;
