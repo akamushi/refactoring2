5f9dc478ad172e572d95d74d170ac1ac9196a884
k_yamamoto <k_yamamoto@ksyssol.co.jp>
2021-06-06 15:03:36 +0900

    volumeCreditsForを移動

--- src/statement.js
+++ src/statement.js
@@ -10,7 +10,7 @@ function statement(invoice, plays) {
     const result = Object.assign({}, aPerformance);
     result.play = playFor(result);
     result.amount = amountFor(result);
-    console.log(result);
+    result.volumeCredits = volumeCreditsFor(result);
     return result;
   }
   function playFor(aPerformance) {
@@ -36,6 +36,15 @@ function statement(invoice, plays) {
       default:
         throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
+    return result;
+  }
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    // ボリューム得点のポイントを加算
+    result += Math.max(aPerformance.audience - 30, 0);
+    // 喜劇の時は10人につき、さらにポイントを加算
+    if ("comedy" === aPerformance.play.type)
+      result += Math.floor(aPerformance.audience / 5);
     return result;
   }
 }
@@ -65,7 +74,7 @@ function renderPlainText(data, plays) {
   function totalVolumeCredits() {
     let result = 0;
     for (let perf of data.performances) {
-      result += volumeCreditsFor(perf);
+      result += perf.volumeCredits;
     }
     return result;
   }
@@ -76,16 +85,6 @@ function renderPlainText(data, plays) {
       currency: "USD",
       minimumFractionDigits: 2,
     }).format(aNumber / 100);
-  }
-
-  function volumeCreditsFor(aPerformance) {
-    let result = 0;
-    // ボリューム得点のポイントを加算
-    result += Math.max(aPerformance.audience - 30, 0);
-    // 喜劇の時は10人につき、さらにポイントを加算
-    if ("comedy" === aPerformance.play.type)
-      result += Math.floor(aPerformance.audience / 5);
-    return result;
   }
 }
 
