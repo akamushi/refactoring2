ebba6063c626c92fadd7f51960c918cb72ffa9b2
k_yamamoto <k_yamamoto@ksyssol.co.jp>
2021-06-09 14:42:36 +0900

    volumeCreditsをクラスに移動

--- src/createStatementData.js
+++ src/createStatementData.js
@@ -44,35 +44,10 @@ class PerformanceCalculator {
     this.play = aPlay;
   }
   get amount() {
-    let result = 0;
-    switch (this.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-
-      default:
-        throw new Error(`unknown type: ${this.play.type}`);
-    }
-    return result;
+    throw new Error(`サブクラスの責務`);
   }
   get volumeCredits() {
-    let result = 0;
-    // ボリューム得点のポイントを加算
-    result += Math.max(this.performance.audience - 30, 0);
-    // 喜劇の時は10人につき、さらにポイントを加算
-    if ("comedy" === this.play.type)
-      result += Math.floor(this.performance.audience / 5);
-    return result;
+    return Math.max(this.performance.audience - 30, 0);
   }
 }
 
@@ -105,6 +80,9 @@ class ComedyCalculator extends PerformanceCalculator {
     }
     result += 300 * this.performance.audience;
     return result;
+  }
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
 module.exports = createStatementData;
